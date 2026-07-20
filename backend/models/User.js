e83af9cc1d63718efd002f922258de68bd241b46
shin6 <rynzexel15@gmail.com>
2026-07-20 21:15:33 +0700

    update product model

--- backend/models/User.js
+++ backend/models/User.js
@@ -14,7 +14,12 @@ const userSchema = new mongoose.Schema(
         password: {
             type: String,
             required: true
-        }
+        },
+        role: {
+            type: String,
+            enum: ['kasir', 'admin'],
+            default: 'kasir'
+        },
     },
     {
         timestamps: true
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -93,6 +93,7 @@ router.post("/login", async (req, res) => {
                 id: user._id,
                 name: user.name,
                 email: user.email,
+                role: user.role,
             },
         });
     } catch (error) {
